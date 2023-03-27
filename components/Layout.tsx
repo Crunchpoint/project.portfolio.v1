a6fd6a4f670dd867245375ee18eb750b9e113c90
Crunchpoint <87291392+Crunchpoint@users.noreply.github.com>
2023-03-28 08:43:21 +0900

    change: route name

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,5 @@
 import styles from "@/styles/Layout.module.scss";
+import Link from "next/link";
 
 type LayoutProps = {
   children: React.ReactNode;
@@ -6,9 +7,20 @@ type LayoutProps = {
 
 const Layout = ({ children }: LayoutProps) => {
   return (
-    //header or navbar
-    <div className={styles.container}>{children}</div>
-    // footer
+    <>
+      <nav className={styles.nav}>
+        <ul className={styles.nav__list}>
+          <li className={styles.nav__item}>
+            <Link href='/about'>어바웃</Link>
+          </li>
+          <li className={styles.nav__item}>스킬</li>
+          <li className={styles.nav__item}>프로젝트</li>
+          <li className={styles.nav__item}>컨택트</li>
+        </ul>
+      </nav>
+      <div className={styles.container}>{children}</div>
+      <footer className={styles.footer}></footer>
+    </>
   );
 };
 
